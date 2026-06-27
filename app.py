be24f710f9283384ebb67aa2b8193f59678bc7d4
kzhzero-afk <kzh.zero@gmail.com>
2026-06-27 18:59:15 +0630

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,18 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 8080))
     )
+    @app.post("/process-video")
+def process_video(req: VideoReq):
+    file_url = req.file_url
+
+    r = requests.get(file_url)
+    if r.status_code != 200:
+        return {"error": "download failed"}
+
+    with open("video.mp4", "wb") as f:
+        f.write(r.content)
+
+    return {
+        "status": "success",
+        "file_url": file_url
+    }
