5f07fd4013fc2850b42f41a462b6539dbe754e39
kzhzero-afk <kzh.zero@gmail.com>
2026-06-27 19:29:16 +0630

    Update app.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-def home():
-    return {"status": "ok"}
+def root():
+    return {
+        "status": "Running",
+        "service": "AI Video Backend"
+    }
+
+if name == "main":
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080))
+    )
